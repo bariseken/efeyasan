2c3e8b3e1eef60473349dc9cc5fde1e01a1974ec
bariseken <55782657+bariseken@users.noreply.github.com>
2020-03-03 23:01:43 +0300

    Start

---
+++ Bounce/Assets/Scripts/Ball.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float throwPower;
+    public Rigidbody rb;
+
+    public Vector3 minPower;
+    public Vector3 maxPower;
+
+    public bool isDragging = false;
+
+    Camera cam;
+    public GameObject playerObject;
+    Vector3 ballForce;
+    Vector3 dragStartPoint;
+    Vector3 dragEndPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            dragStartPoint = (Input.mousePosition);
+            Debug.Log(dragStartPoint);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            isDragging = true;
+            Vector3 currentPoint = (Input.mousePosition);
+            Debug.Log(currentPoint);
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            
+            isDragging = false;
+
+            dragEndPoint = (Input.mousePosition);
+
+            ballForce = new Vector3(Mathf.Clamp(dragStartPoint.x - dragEndPoint.x, minPower.x, maxPower.x), Mathf.Clamp(dragStartPoint.y - dragEndPoint.y, minPower.y, maxPower.y),0);
+
+            Debug.Log(dragEndPoint);
+
+            rb.AddForce(ballForce * throwPower, ForceMode.Force);
+        }
+    }
+
+
+}
---
+++ Bounce/Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject top;
+
+    public float minPower = 1f;
+    public float maxPower = 5f;
+    public float chargeUpDuration = 2f;
+    public float chargeDurationNow = 0f;
+    public bool isCharging = false;
+    public bool buttonReleased = true;
+
+    Vector3 startDrag;
+    Vector3 endDrag;
+    Vector3 currentDragPos;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)&& buttonReleased)
+        {
+            chargeDurationNow = 0f;
+            isCharging = true;
+            buttonReleased = false;
+        }
+
+        else if(isCharging && (Input.GetMouseButtonUp(0)|| chargeDurationNow >= chargeUpDuration))
+        {
+            float throwPower = Mathf.Lerp(minPower, maxPower, chargeDurationNow / chargeUpDuration);
+
+            BallThrow(throwPower);
+
+            Reset();
+        }
+
+        if (isCharging)
+        {
+            chargeDurationNow += Time.deltaTime;
+        }
+
+        if (!buttonReleased)
+        {
+            buttonReleased = Input.GetMouseButtonUp(0);
+        }
+    }
+
+    public void BallThrow(float throwPower)
+    {
+        Debug.Log("guc: " + throwPower);
+        if( top != null)
+        {
+            top.GetComponent<Rigidbody>().AddForce(top.transform.up * throwPower, ForceMode.Impulse);
+        }
+        else
+        {
+            Debug.Log("top bulunamadı");
+        }
+    }
+
+    private void Reset()
+    {
+        chargeDurationNow = 0f;
+        isCharging = false;
+    }
+
+
+}
